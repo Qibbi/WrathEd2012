be4856fb9fc86e062560d023ba30700556a4ec25
Jana <ingoneur@gmx.de>
2021-05-17 00:43:47 +0200

    Fixed EntryPoidType for StringHash

--- SAGE/EntryPoidType.cs
+++ SAGE/EntryPoidType.cs
@@ -1,64 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Files;
+using System;
 using System.Xml;
-using Files;
 
 namespace SAGE
 {
-	public class EntryPoidType : BaseEntryType
-	{
-		public string Description { get; protected set; }
-		public bool IsUpperCase { get; protected set; }
+    public class EntryPoidType : BaseEntryType
+    {
+        public string Description { get; protected set; }
+        public bool IsUpperCase { get; protected set; }
 
-		public EntryPoidType(WrathEdXML.AssetDefinition.EntryPoidType entry)
-			: base(entry)
-		{
-			Description = entry.Description;
-			IsUpperCase = entry.IsUpperCase;
-		}
+        public EntryPoidType(WrathEdXML.AssetDefinition.EntryPoidType entry)
+            : base(entry)
+        {
+            Description = entry.Description;
+            IsUpperCase = entry.IsUpperCase;
+        }
 
-		public override bool Compile(Uri baseUri, BinaryAsset asset, XmlNode node, GameDefinition game, string trace, ref int position, out string ErrorDescription)
-		{
-			bool isFound = false;
-			if (IsAttribute)
-			{
-				foreach (XmlAttribute attribute in node.Attributes)
-				{
-					if (attribute.Name == id)
-					{
-						isFound = true;
-						FileHelper.SetUInt(StringHasher.Hash(((IsUpperCase) ? attribute.Value : attribute.Value.ToLowerInvariant())), position, asset.Content);
-						break;
-					}
-				}
-			}
-			else
-			{
-				foreach (XmlNode childNode in node.ChildNodes)
-				{
-					if (childNode.Name == id)
-					{
-						isFound = true;
-						FileHelper.SetUInt(StringHasher.Hash(((IsUpperCase) ? childNode.InnerXml : childNode.InnerXml.ToLowerInvariant())), position, asset.Content);
-						break;
-					}
-				}
-			}
-			if (!isFound && IsRequired)
-			{
-				if (IsAttribute)
-				{
-					ErrorDescription = string.Format(RequiredNotFoundAttribute, id, trace);
-				}
-				else
-				{
-					ErrorDescription = string.Format(RequiredNotFoundElement, id, trace);
-				}
-				return false;
-			}
-			position += 4;
-			ErrorDescription = string.Empty;
-			return true;
-		}
-	}
+        public override bool Compile(Uri baseUri, BinaryAsset asset, XmlNode node, GameDefinition game, string trace, ref int position, out string ErrorDescription)
+        {
+            bool isFound = false;
+            if (IsAttribute)
+            {
+                foreach (XmlAttribute attribute in node.Attributes)
+                {
+                    if (attribute.Name == id)
+                    {
+                        isFound = true;
+                        FileHelper.SetUInt(StringHasher.Hash(((IsUpperCase) ? attribute.Value : attribute.Value.ToLowerInvariant())), position, asset.Content);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                foreach (XmlNode childNode in node.ChildNodes)
+                {
+                    if (childNode.Name == "#text")
+                    {
+                        isFound = true;
+                        FileHelper.SetUInt(StringHasher.Hash(((IsUpperCase) ? childNode.InnerText : childNode.InnerText.ToLowerInvariant())), position, asset.Content);
+                        break;
+                    }
+                    if (childNode.Name == id)
+                    {
+                        isFound = true;
+                        FileHelper.SetUInt(StringHasher.Hash(((IsUpperCase) ? childNode.InnerXml : childNode.InnerXml.ToLowerInvariant())), position, asset.Content);
+                        break;
+                    }
+                }
+            }
+            if (!isFound && IsRequired)
+            {
+                if (IsAttribute)
+                {
+                    ErrorDescription = string.Format(RequiredNotFoundAttribute, id, trace);
+                }
+                else
+                {
+                    ErrorDescription = string.Format(RequiredNotFoundElement, id, trace);
+                }
+                return false;
+            }
+            position += 4;
+            ErrorDescription = string.Empty;
+            return true;
+        }
+    }
 }
